92c492cb7a0f410328f8cf85b7202cf5def0a3f7
chrislyr0420 <li.yiran@wustl.edu>
2020-10-09 16:31:25 +0800

    Add Router in Main.js, now register is in /register
    change TopBar style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { Register } from './Register'
 
 class Main extends React.Component {
     
     render() {
-        return <div className="main">
-            <Register />
-        </div>
+        return (
+            <div className="main">
+                <Switch>
+                    <Route path="/register" component={Register}/>
+                </Switch>
+            </div>
+        );
     };
 }
 
