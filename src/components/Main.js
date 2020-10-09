74e48f3bbaf07ed77baff9cb16177564d98a4e17
chrislyr0420 <li.yiran@wustl.edu>
2020-10-09 20:34:03 +0800

    Finish pass down tokey logic to handle login customer redirect to Home page logic
    In Main.js, for more details about why use alt `render` rather than `component` in Route: https://www.cnblogs.com/cangqinglang/p/12742159.html
    
    (In short, it avoid multi mounting problem)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { Register } from './Register';
 import { Login } from './Login';
 import Home from './Home';
 
 class Main extends React.Component {
-    
+    getLogin = () => {
+        return this.props.isLoggedIn 
+        ? <Redirect to="/Home" /> 
+        : <Login handleLoginSucceed={this.props.handleLoginSucceed} />
+    }
+
     render() {
         return (
             <div className="main">
                 <Switch>
                     <Route path="/register" component={Register}/>
-                    <Route path="/login" component={Login}/>
-                    <Route path="/home" component={Home}/>
+                    <Route path="/login" render={this.getLogin}/>
                 </Switch>
             </div>
         );
